6c576f1542e4f277bd64e58b1792b914c13812f7
barryblando <barryblando@gmail.com>
2022-07-20 21:29:00 +0800

    add env

---
+++ config/custom-environment-variables.ts
@@ -0,0 +1,9 @@
+export default {
+  dbName: "MONGODB_DATABASE_NAME",
+  dbUser: "MONGODB_USERNAME",
+  dbPass: "MONGODB_PASSWORD",
+  accessTokenPrivateKey: "ACCESS_TOKEN_PRIVATE_KEY",
+  accessTokenPublicKey: "ACCESS_TOKEN_PUBLIC_KEY",
+  refreshTokenPrivateKey: "REFRESH_PRIVATE_KEY",
+  refreshTokenPublicKey: "REFRESH_PUBLIC_KEY",
+};
