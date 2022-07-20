b13a7b67a0de6120b5539fe0c00e3f8a81b64957
barryblando <barryblando@gmail.com>
2022-07-20 21:29:09 +0800

    update default

--- config/default.ts
+++ config/default.ts
@@ -1,15 +1,12 @@
 export default {
   port: 3001,
-  dbUri: "mongodb://localhost:27017/auth-api",
+  dbHost: "localhost:27017",
   loglevel: "info",
-  accessTokenPrivateKey: process.env.ACCESS_TOKEN_PRIVATE_KEY,
-  accessTokenPublicKey: process.env.ACCESS_TOKEN_PUBLIC_KEY,
-  refreshTokenPrivateKey: process.env.REFRESH_PRIVATE_KEY,
-  refreshTokenPublicKey: process.env.REFRESH_PUBLIC_KEY,
+  origin: 'http://localhost:3000',
   // test mail server credentials
   smtp: {
-    user: 'l37cayndf2ojt2kz@ethereal.email',
-    pass: '17HCqBAGZeSQhtFEhn',
+    user: 'fobpudj62qdr5shc@ethereal.email',
+    pass: 'crDYz7hmCuYfwyjUWM',
     host: 'smtp.ethereal.email',
     port: 587 ,
     secure: false,
