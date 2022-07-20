c6bb8898c6b7018c27eae750a55552555d239fe0
barryblando <barryblando@gmail.com>
2022-07-20 21:32:11 +0800

    update

--- src/middleware/requireUser.ts
+++ src/middleware/requireUser.ts
@@ -1,14 +1,15 @@
 import { Request, Response, NextFunction } from 'express'
+import AppError from '../utils/appError'
 
 const requireUser = (
-  req: Request,
+  _req: Request,
   res: Response,
   next: NextFunction
 ) => {
   const user = res.locals.user
 
   if (!user) {
-    return res.sendStatus(403)
+    return next(new AppError("Invalid token or session has expired", 403))
   }
 
   return next()
