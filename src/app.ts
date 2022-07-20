10d3aadffc54ebbc7482af6451d977247b6ba8f2
barryblando <barryblando@gmail.com>
2022-07-20 21:32:34 +0800

    update app

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,5 @@
-require('dotenv').config()
-import express from 'express'
-import config from 'config'
-import connectToDb from './utils/connectToDb'
-import log from './utils/logger'
+import express, { NextFunction, Request, Response } from 'express'
+// import cors from 'cors'
 import router from './routes'
 import deserializeUser from './middleware/deserializeUser'
 
@@ -10,15 +7,35 @@ const app = express()
 
 app.use(express.json())
 
+// set current user to routes that need authentication
 app.use(deserializeUser)
+
+// app.use(
+//   cors({
+//     origin: config.get<string>('origin'),
+//     credentials: true,
+//   })
+// );
 
 app.use(router)
 
-const port = config.get('port')
+// UnKnown Routes
+app.use((req: Request, _res: Response, next: NextFunction) => {
+  const err = new Error(`Route ${req.originalUrl} not found`) as any;
+  err.statusCode = 404;
+  next(err);
+});
 
-app.listen(port, () => {
-  log.info(`App started at http://localhost:${port}`)
+// Global Error Handler
+app.use((err: any, _req: Request, res: Response, _next: NextFunction) => {
+  err.status = err.status || 'error'
+  err.statusCode = err.statusCode || 500
 
-  connectToDb()
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message
+  })
 })
+
+export { app }
 
