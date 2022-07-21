4c9c44e192652753341f47ee2738ed50a5b32d12
barryblando <barryblando@gmail.com>
2022-07-21 11:39:39 +0800

    update auth service

--- src/service/auth.service.ts
+++ src/service/auth.service.ts
@@ -1,20 +1,22 @@
+import { Response } from 'express'
 import { DocumentType } from '@typegoose/typegoose'
-import { omit } from 'lodash'
-import SessionModel from '../model/session.model'
-import { privateFields, User } from '../model/user.model'
+import SessionModel, { Session } from '../model/session.model'
+import { User } from '../model/user.model'
 import { signJwt } from '../utils/jwt'
 
-export async function createSession({ userId }: { userId: string }) {
-  return SessionModel.create({ user: userId })
+export async function createSession({ userId, userAgent, ip }: Partial<DocumentType<Session>>) {
+  return SessionModel.create({ userId, userAgent, ip })
 }
 
 export async function findSessionById(id: string) {
   return SessionModel.findById(id)
 }
 
-export async function signRefreshToken({ userId }: { userId: string }) {
+export async function signRefreshToken({ userId, userAgent, ip }: Partial<DocumentType<Session>>) {
   const session = await createSession({
-    userId
+    userId,
+    userAgent,
+    ip
   })
 
   const refreshToken = signJwt(
@@ -27,8 +29,8 @@ export async function signRefreshToken({ userId }: { userId: string }) {
 }
 
 export function signAccessToken(user: DocumentType<User>) {
-  const payload = omit(user.toJSON(), privateFields)
-
+  const payload = user.toJSON()
+  
   const accessToken = signJwt(
     payload,
     "accessTokenPrivateKey",
@@ -36,4 +38,27 @@ export function signAccessToken(user: DocumentType<User>) {
   ) 
   
   return accessToken
+}
+
+// https://learning.postman.com/docs/sending-requests/cookies/
+// res.cookie('jwt', token, {
+// 	httpOnly: true, // so it can't be accessed by javascript xss attacks
+//	secure: true, //on HTTPS
+//	domain: 'example.com', // set your domain, if you want it to work w/ sub domain put '.example.com'
+// `sub domain i.e api.example.com, www.example.com` - this is important if you're using using SSR (Next.js/Nuxt.js)
+// })
+
+export function sendRefreshToken(res: Response, token: string) {
+	res.cookie('session', token, {
+		expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000), //  match the expiration date use in the refresh token (7days)
+		httpOnly: true,
+		path: '/api/sessions/',
+	})
+}
+
+// https://expressjs.com/en/5x/api.html#res.clearCookie
+export function clearCookie(res: Response) {
+	res.clearCookie('session', {
+		path: '/api/sessions/',
+	})
 }
