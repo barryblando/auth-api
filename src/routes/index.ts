fb207473d0f6f3719d5bf1fb758c26320f8492c0
barryblando <barryblando@gmail.com>
2022-07-21 11:38:55 +0800

    update index route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,21 @@
-import express from 'express'
+import express, { NextFunction, Request, Response } from 'express'
 import user from './user.routes'
 import auth from './auth.routes'
 
 const router = express.Router()
 
-router.get('/healthcheck', (_, res) => res.sendStatus(200))
+// Testing
+router.get(
+  '/api/healthcheck',
+  (req: Request, res: Response, _next: NextFunction) => {
+    res.status(200).json({
+      status: 'success',
+      message: 'Ok',
+      ip: req.ip,
+      userAgent: req.headers["user-agent"]
+    })
+  }
+)
 
 router.use(user)
 router.use(auth)
