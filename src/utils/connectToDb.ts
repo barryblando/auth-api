b5a781cccf303dbf7f88ffc69083af4c56dc0291
barryblando <barryblando@gmail.com>
2022-07-21 13:24:58 +0800

    update

--- src/utils/connectToDb.ts
+++ src/utils/connectToDb.ts
@@ -2,11 +2,18 @@ import mongoose from 'mongoose'
 import config from 'config'
 import log from './logger'
 
+const dbUser = config.get('dbUser')
+const dbPass = config.get('dbPass')
+const dbName = config.get('dbName')
+const dbHost = config.get('dbHost')
+
+// https://stackoverflow.com/questions/40608669/what-does-authsource-means-in-mongo-database-url
+// https://www.mongodb.com/docs/manual/reference/connection-string/#std-label-connections-standard-connection-string-format
+const dbUrl = `mongodb://${dbUser}:${dbPass}@${dbHost}/${dbName}?authSource=admin`
+
 async function connectToDb() {
-  const dbUri = config.get<string>('dbUri')
-  
   try { 
-    await mongoose.connect(dbUri)
+    await mongoose.connect(dbUrl)
     log.info('Connected to DB')
   } catch (e) {
     process.exit(1)
