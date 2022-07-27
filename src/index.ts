f455ccb64edd62e9697dfef721967cd3e6199bb4
barryblando <barryblando@gmail.com>
2022-07-27 20:32:44 +0800

    add startMetricServer

--- src/index.ts
+++ src/index.ts
@@ -4,12 +4,15 @@ import config from 'config'
 import connectMongo from './utils/connectMongo'
 import log from './utils/logger'
 import { app } from './app'
+import { startMetricServer } from './utils/metrics'
 
 const start = async () => {
   const port = config.get('port')
   
   app.listen(port, async () => {
     await connectMongo()
+    
+    startMetricServer()
     
     log.info(`Server started at http://localhost:${port}`)
   })
